781ae25e008e40e9c6777945872a3ab08a5705fb
Ran <122150955+RanP90@users.noreply.github.com>
2024-11-13 19:11:50 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,8 +5,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ExoticPlantsContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("ExoticPlantsDatabase")));
+
+// Configure DbContext to load the connection string only in Development environment
+string connectionString;
+if (builder.Environment.IsDevelopment())
+{
+    // Retrieve the connection string from appsettings.Development.json
+    connectionString = builder.Configuration.GetConnectionString("ExoticPlantsDatabase");
+}
+else
+{
+   
+    connectionString = null; 
+}
+
+if (!string.IsNullOrEmpty(connectionString))
+{
+    builder.Services.AddDbContext<ExoticPlantsContext>(options =>
+        options.UseSqlite(connectionString));
+}
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -20,8 +38,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
-// This line maps routes from controller attributes
 app.MapControllers();
-
 app.Run();
